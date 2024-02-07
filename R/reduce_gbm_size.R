2f189f99cbadfd04d26a166cde61980225549f6c
Alex Zajichek <alexzajichek@Alexs-MacBook-Pro.local>
2024-02-07 14:30:53 -0600

    Few additions

--- R/reduce_gbm_size.R
+++ R/reduce_gbm_size.R
@@ -23,11 +23,24 @@ reduced_mod <- mod[setdiff(names(mod), "fit")]
 class(reduced_mod) <- "gbm"
 
 # What are the object sizes?
-object.size(mod) # 8101360 bytes
-object.size(reduced_mod) # 101240 bytes
+object.size(mod) # 8.1 MB
+object.size(reduced_mod) # 0.10 MB
 
-# Do they produce the same predictions?
+# Save to disk for later use
+save(mod, file = "mod.RData") # 1.8 MB
+save(reduced_mod, file = "reduced_mod.RData") # 9 KB
+
+## Do they produce the same predictions?
+
+# Load the models from disk
+load(file = "mod.RData")
+load(file = "reduced_mod.RData")
+
+# Get predictions
 n.trees <- mod$n.trees
 pred_mod <- predict(mod, newdata = dat, n.trees = n.trees, type = "response")
 pred_reduced_mod <- predict(reduced_mod, newdata = dat, n.trees = n.trees, type = "response")
 all.equal(pred_mod, pred_reduced_mod) # TRUE
+
+# Caveat: Trying to do this with multiple models in lists, etc. creates environment overhead when trying to save to an .RData file, so you lose the size benefit
+# Fix: Define models on their own
